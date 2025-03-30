81f8285db3ef5a1a3ccfbd581cb0a27daa223d22
NullPtr-rs <nullptr.rs@outlook.com>
2025-03-30 01:31:44 +0100

    Change filenames and add playlist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/nullptr-rs-player/',
   plugins: [
     react(),
     VitePWA({
